ceb3a809570ff0fad4efa45a3d1365c4afed5602
fculibao <34431067+fculibao@users.noreply.github.com>
2021-09-26 21:57:49 -0400

    Update aws-tf-iac-main.tf

--- aws-tf-iac-main.tf
+++ aws-tf-iac-main.tf
@@ -58,28 +58,28 @@ resource "aws_security_group" "web02-prod-allow-http-https-traffic" {
   description = "Allow HTTP inbound traffic"
   vpc_id      = aws_vpc.web02-prod.id
 
-  ingress = {
+  ingress {
       description      = "HTTPS from VPC"
       from_port        = 443
       to_port          = 443
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
   }
-  ingress = {
+  ingress {
       description      = "HTTP from VPC"
       from_port        = 80
       to_port          = 80
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
   }
-  ingress = {
+  ingress {
       description      = "SSH from VPC"
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
   }
-  egress = {
+  egress {
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
