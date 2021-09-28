e107344bc4943959647c6c0e1c7705102d7bd8c3
Ferdie Culibao <ferdie.culibao@stackpath.com>
2021-09-27 23:13:29 -0400

    Resolving Issue

--- aws-tf-iac-02-main.tf
+++ aws-tf-iac-02-main.tf
@@ -53,9 +53,6 @@ resource "aws_route_table_association" "a" {
   subnet_id = aws_subnet.web02-prod-subnet.id
   route_table_id = aws_route_table.web02-prod-route-table.id
 
-  tags = {
-    Name = var.tag_name
-  }
 }
 
 
