4acecc75acc5f6c02199a6c6ff86e3da3eb8fd08
Ferdie Culibao <ferdie.culibao@stackpath.com>
2021-09-27 21:35:26 -0400

    Edit api-data.sh

--- api-data.sh
+++ api-data.sh
@@ -7,12 +7,12 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 sudo bash -c 'echo your very first web server > /var/www/html/index.html'
 
-# Install Docker on Ubuntu
-sudo apt update
+# Install Docker on Ubuntu from official Repository
+sudo apt update -y 
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
-sudo apt update
+sudo apt update -y
 sudo apt-get install docker-ce -y
 sudo systemctl start docker
 sudo systemctl enable docker
