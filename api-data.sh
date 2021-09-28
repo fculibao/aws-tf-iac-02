3a7325c420a1ea55c9e57b4030542894501e9183
Ferdie Culibao <ferdie.culibao@stackpath.com>
2021-09-27 21:08:47 -0400

    Edited api-data.sh for installing docker

--- api-data.sh
+++ api-data.sh
@@ -4,12 +4,26 @@
 sudo apt update -y
 sudo apt install apache2 -y
 sudo systemctl start apache2
+sudo systemctl enable apache2
 sudo bash -c 'echo your very first web server > /var/www/html/index.html'
 
-# Install Docker
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo yum install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
+# Install Docker on Ubuntu
+sudo apt update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+sudo apt update
+sudo apt-get install docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+
+
+
+
+# Installing Docker on Amazon Linux
+#sudo yum update -y
+#sudo amazon-linux-extras install docker
+#sudo yum install docker
+#sudo service docker start
+#sudo usermod -a -G docker ec2-user
 
