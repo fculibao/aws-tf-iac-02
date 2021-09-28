4259f736fce56142484adfc6c424217ce54ca1c0
Ferdie Culibao <ferdie.culibao@stackpath.com>
2021-09-27 20:53:40 -0400

    Add docker install into our api-data.sh script

--- api-data.sh
+++ api-data.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+# Install apache2
 sudo apt update -y
 sudo apt install apache2 -y
 sudo systemctl start apache2
 sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+
+# Install Docker
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo yum install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
